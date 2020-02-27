4c05a26035270b327ecb19050bb27bf387d5e6c1
GitHub Action <action@github.com>
2020-02-27 09:40:46 +0000

    GitHub Action's build

--- app/geckoModule/waveSurferEvents/pause.js
+++
@@ -1,5 +0,0 @@
-export default (parent) => {
-    parent.olatsNode && parent.olatsNode.disconnect()
-    parent.isPlaying = false
-    parent.$scope.$evalAsync()
-}
--- app/geckoModule/waveSurferEvents/seek.js
+++
@@ -1,8 +0,0 @@
-export default (parent) => {
-    parent.updateView()
-    parent.videoPlayer && parent.videoPlayer.currentTime(parent.wavesurfer.getCurrentTime())
-
-    parent.cursorRegion = parent.getCurrentRegion(parent.selectedFileIndex)
-
-    parent.$scope.$evalAsync()
-}
